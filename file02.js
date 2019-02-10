5c5a1030426a816f9f22e41c79f4755dd94d89a8
Puneet <birmaan.puneet19@gmail.com>
2019-02-10 13:05:13 +0530

    add nice to have you
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

--- file02.js
+++ file02.js
@@ -2,12 +2,12 @@ function greet(name){
     console.log("knock knock knock " + name)
     console.log("knock knock knock " + name)
     console.log("knock knock knock " + name)
-
+  console.log('Nice to have you here !')
 }
 greet("Puneet")
-<<<<<<< HEAD
-=======
+// HEAD
+// =======
 
 console.log('use "git status" to  check status')
 console.log('use "git log" to  check history')
->>>>>>> gitlearning
+// >>>>>>> gitlearning
