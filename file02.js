b49318864c281b706b0dbfd9b9463e63f4f0a71d
Puneet <birmaan.puneet19@gmail.com>
2019-02-10 11:17:42 +0530

    more git info added to file02.js

--- file02.js
+++ file02.js
@@ -2,3 +2,6 @@ function greet(name){
     console.log("hey " + name + "! we're learning git")
 }
 greet("Puneet")
+
+console.log('use "git status" to  check status')
+console.log('use "git log" to  check history')
